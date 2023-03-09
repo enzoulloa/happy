e65e8209dfdecf1fbad59766afff92aa961a98e5
enzoulloa <enzo_ulloa.i@gmail.com>
2023-03-09 04:37:03 -0300

    feat: :mag: add href to anchor tags

--- src/components/DesktopNavbar.jsx
+++ src/components/DesktopNavbar.jsx
@@ -8,21 +8,21 @@ const DesktopNavbar = () => {
           <img src={navLogo} alt='navLogo' loading='eager' />
         </a>
         <div className=' flex h-full w-auto items-center '>
-          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href=''>
+          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href='#features'>
             Features
           </a>
-          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href=''>
+          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href='#pricing'>
             Pricing
           </a>
-          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href=''>
+          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href='#testimonials'>
             Testimonials
           </a>
-          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href=''>
-            Instruction
+          <a className=' mx-0 p-5 px-4 text-sm text-gray-500 transition-all ease-in duration-200' href='https://www.linkedin.com/in/enzo-ulloa-2639a1194/' target='_blank' rel='noreferrer'>
+            Me
           </a>
           <a
             className=' my-2 mx-1 ml-2 cursor-pointer rounded-md bg-secondary py-3 px-6 text-center text-sm uppercase leading-5 tracking-widest text-primary transition-all ease-in duration-200'
-            href=''
+            href='https://webflow.com/?r=0' target='_blank' rel='noreferrer'
           >
             GET TEMPLATE
           </a>
